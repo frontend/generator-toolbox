db8c01634571ce70b31de531f8e402166931c23f
Toni Fisler <mail@tonifisler.ch>
2015-04-28 07:43:58 +0200

    add task to minify svgs and optimize images

--- app/templates/tasks/images.js
+++ app/templates/tasks/images.js
@@ -2,13 +2,35 @@
 
 module.exports = function(gulp, $, config) {
 
+  var pngquant = require('imagemin-pngquant');
+
  /**
   * Copy images
   */
-  gulp.task('img', function() {
+  gulp.task('img-optim', function() {
     return gulp.src(config.images)
+      .pipe($.imagemin({
+        progressive: true,
+        use: [pngquant()]
+      }))
       .pipe($.size({title: 'IMAGES'}))
-      .pipe(gulp.dest(config.app.buildpath + 'img'));
+      .pipe(gulp.dest(config.build + 'img'));
   });
+
+  /**
+   * Copy svg
+   */
+   gulp.task('svg-optim', function() {
+     return gulp.src(config.svg)
+       .pipe($.imagemin({
+        svgoPlugins: [{
+          cleanupIDs: false // we usually need them
+        }]
+       }))
+       .pipe($.size({title: 'SVG'}))
+       .pipe(gulp.dest(config.build + 'svg'));
+   });
+
+   gulp.task('img', ['img-optim', 'svg-optim']);
 
 };
