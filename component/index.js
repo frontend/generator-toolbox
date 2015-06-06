1d0e485662e1c4c92965fc1758c128465f3c0bab
Yann Gouffon <github@yago-art.ch>
2015-06-06 11:17:20 +0200

    test if gulp_config exist before prompt

--- component/index.js
+++ component/index.js
@@ -4,67 +4,79 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var slug = require('slug');
+var fs = require('fs');
 
 module.exports = yeoman.generators.Base.extend({
 
   initializing: function () {
-    this.config = require(this.destinationPath('gulp_config.json'));
+    if(fs.existsSync(this.destinationPath('gulp_config.json'))){
+      this.config = require(this.destinationPath('gulp_config.json'));
+      this.continue = true;
+    } else {
+      this.log(chalk.red("No gulp_config.json founded !") + "\nMake sure that the gulp_config.json is at your project's root.")
+      this.continue = false;
+    }
   },
 
   prompting: function () {
     var done = this.async();
     this.slug = slug;
 
-    var prompts = [{
-      type: 'list',
-      name: 'type',
-      message: 'What kind of component is it ? ',
-      choices: [{
-          name: 'Atom',
-          value: 'atoms',
-          checked: true
-        }, {
-          name: 'Molecule',
-          value: 'molecules',
-          checked: false
-        }, {
-          name: 'Organism',
-          value: 'organisms',
-          checked: false
-        }, {
-          name: 'Template',
-          value: 'templates',
-          checked: false
-        }
-      ]
-    },{
-      type: 'input',
-      name: 'name',
-      message: 'What\'s the name of your component ?',
-      default: 'Toolbox'
-    }];
+    if (this.continue) {
+      var prompts = [{
+        type: 'list',
+        name: 'type',
+        message: 'What kind of component is it ? ',
+        choices: [{
+            name: 'Atom',
+            value: 'atoms',
+            checked: true
+          }, {
+            name: 'Molecule',
+            value: 'molecules',
+            checked: false
+          }, {
+            name: 'Organism',
+            value: 'organisms',
+            checked: false
+          }, {
+            name: 'Template',
+            value: 'templates',
+            checked: false
+          }
+        ]
+      },{
+        type: 'input',
+        name: 'name',
+        message: 'What\'s the name of your component ?',
+        default: 'Toolbox'
+      }];
 
-    this.prompt(prompts, function (props) {
-      this.type = props.type;
-      this.name = props.name;
+      this.prompt(prompts, function (props) {
+        this.type = props.type;
+        this.name = props.name;
 
+        done();
+      }.bind(this));
+    } else {
       done();
-    }.bind(this));
+    }
   },
 
   writing: function () {
-    if (this.type !== 'templates') {
-      this.template('_component.hbs', this.config.assets + 'components/'+ this.type +'/'+ slug(this.name).toLowerCase() +'.hbs');
-    } else {
-      this.template('component.html', this.config.assets + 'templates/'+ slug(this.name).toLowerCase() +'.html');
+    if (typeof this.name !== 'undefined' && typeof this.type !== 'undefined') {
+      if (this.type !== 'templates') {
+        this.template('_component.hbs', this.config.assets + 'components/'+ this.type +'/'+ slug(this.name).toLowerCase() +'.hbs');
+      } else {
+        this.template('component.html', this.config.assets + 'templates/'+ slug(this.name).toLowerCase() +'.html');
+      }
+
+      var plural = '';
+      if (this.type !== 'templates') {
+        plural = 's';
+      }
+
+      this.copy('components.scss', this.config.assets + 'sass/'+ this.type +'/_'+ slug(this.name).toLowerCase() + plural +'.scss');
     }
-
-    var plural = '';
-    if (this.type !== 'templates') {
-      plural = 's';
-    }
-
-    this.copy('components.scss', this.config.assets + 'sass/'+ this.type +'/_'+ slug(this.name).toLowerCase() + plural +'.scss');
-
   }
 });
