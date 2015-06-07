e33e039882cdf241950f98d01668b6c0a41d5656
Yann Gouffon <github@yago-art.ch>
2015-06-07 20:27:19 +0200

    a more flexible component format watched

--- app/templates/tasks/_server.js
+++ app/templates/tasks/_server.js
@@ -36,7 +36,7 @@ module.exports = function() {
       runSequence('scripts'<% if (fabricator) { %>, 'styleguide'<% } %>, reload);
     });<% if (fabricator) { %>
     gulp.watch([
-      config.assets + 'components/**/*.html',
+      config.assets + 'components/**/*.{html,hbs,md}',
       config.assets + 'templates/**/*.html',
       config.assets + 'docs/**/*.md',
       config.assets + 'data/**/*.{json,yml}'
