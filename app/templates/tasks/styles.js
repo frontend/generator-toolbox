7268e92695b15d2f0a185ca56e529a6c10c0e6c4
Toni Fisler <mail@tonifisler.ch>
2016-09-21 13:43:39 +0200

    lint

--- app/templates/tasks/styles.js
+++ app/templates/tasks/styles.js
@@ -1,21 +1,17 @@
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
-import config from '../gulp_config.json';
 import yargs from 'yargs';
-import slug from 'slug';
 import autoprefixer from 'autoprefixer';
-import stylelint from 'stylelint';
+// required -> import stylelint from 'stylelint';
 import reporter from 'postcss-reporter';
 import scss from 'postcss-scss';
+import config from '../gulp_config.json';
 
 import loadPlugins from 'gulp-load-plugins';
 const $ = loadPlugins();
 
-const iconFontName = slug(config.iconsFontName).toLowerCase();
-
-function errorAlert(error){
+function errorAlert(error) {
   if (!yargs.argv.production) {
-    $.notify.onError({title: 'SCSS Error', message: 'Check your terminal', sound: 'Sosumi'})(error);
+    $.notify.onError({ title: 'SCSS Error', message: 'Check your terminal', sound: 'Sosumi', })(error);
     $.util.log(error.messageFormatted ? error.messageFormatted : error.message);
   }
   this.emit('end');
@@ -26,38 +22,37 @@ function errorAlert(error){
  * With error reporting on compiling (so that there's no crash)
  */
 export const stylesBuild = () => {
-  if (yargs.argv.production) { $.util.log('[styles] Production mode' ); }
-  else { $.util.log('[styles] Dev mode'); }
+  if (yargs.argv.production) { $.util.log('[styles] Production mode'); } else { $.util.log('[styles] Dev mode'); }
 
   return gulp.src([`${config.assets}sass/main.scss`])
-    .pipe($.plumber({errorHandler: errorAlert}))
+    .pipe($.plumber({ errorHandler: errorAlert }))
     .pipe(yargs.argv.production ? $.util.noop() : $.sourcemaps.init())
     .pipe($.sass({
       outputStyle: 'compressed',
       precision: 5,
-      includePaths: ['.']
+      includePaths: ['.'],
     }))
     .pipe($.postcss([
       autoprefixer({
         browsers: config.browsers,
         options: {
-          map: true
+          map: true,
         }
       })
     ]))
     .pipe(yargs.argv.production ? $.util.noop() : $.sourcemaps.write())
-    .pipe(yargs.argv.production ? $.cleanCss() : $.util.noop() )
+    .pipe(yargs.argv.production ? $.cleanCss() : $.util.noop())
     .pipe($.concat('main.css'))
-    .pipe($.size({title: 'STYLES', showFiles: true}))
+    .pipe($.size({ title: 'STYLES', showFiles: true }))
     .pipe(gulp.dest(`${config.build}/css`));
 };
 
 export const stylesLint = () => {
   return gulp.src([`${config.assets}sass/**/*.s+(a|c)ss`])
-      .pipe($.plumber({errorHandler: errorAlert}))
+      .pipe($.plumber({ errorHandler: errorAlert }))
       .pipe($.postcss(
         [
-          stylelint(),
+          require('stylelint')(),
           reporter({
             clearMessages: true,
             throwError: yargs.argv.production
