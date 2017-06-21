662994a3d0cdb9d241c7a6084bd8f62bbf953bad
Toni Fisler <mail@tonifisler.ch>
2017-06-21 09:37:34 +0200

    remove console.log (pretext to test update-notifier)

--- generators/app/index.js
+++ generators/app/index.js
@@ -33,8 +33,6 @@ module.exports = class extends Generator {
     const notifier = updateNotifier({pkg});
     // Notify using the built-in convenience method
     notifier.notify();
-    // `notifier.update` contains some useful info about the update
-    console.log(notifier.update);
   }
 
   prompting() {
