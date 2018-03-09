c258439fa43f7a2982d1a0bab0e868153ba2ba06
Yann Gouffon <github@yago-art.ch>
2018-03-09 10:13:38 +0100

    ✨ better script initialization

--- generators/app/templates/assets/base.js
+++ generators/app/templates/assets/base.js
@@ -1,12 +1,17 @@
+/* globals jQuery, document */
+
 // You will use that file to import all your scripts
 // Ex: import gallery from './gallery'<% if (svgIcons) { %>
 import svgIcons from '../icons/svg-icons';
 
 svgIcons(); // Must run as soon as possible<% } %>
 
+const init = () => {
+  // Run your imported scripts
+  // Ex: gallery();
+};
+
 (function ($) {
-  $(document).ready(function () {
-    // Run your imported scripts
-    // Ex: gallery();
-  });
+  $(document).ready(() => init());
 })(jQuery);
+document.addEventListener('ToolboxReady', () => init());
