c623497cbf7405af1f82857457dd1f1e66a0b1c3
knuch <knuchelf@gmail.com>
2019-03-19 13:21:35 +0100

    avoid inserting markup in page when icons fail to load

--- generators/app/templates/assets/svg-icons.js
+++ generators/app/templates/assets/svg-icons.js
@@ -6,7 +6,9 @@ const svgIcons = () => {
   ajax.onload = function (e) {
     var div = document.createElement('div');
     div.innerHTML = ajax.responseText;
-    document.body.insertBefore(div, document.body.childNodes[0]);
+    if (ajax.status === 200) {
+      document.body.insertBefore(div, document.body.childNodes[0]);
+    }
   };
 };
 
